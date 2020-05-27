b7a57eb717b3929880007b2aed5b7a3800a93d9f
Edu003 <tr0.eduardo@gmail.com>
2020-05-26 21:49:08 -0600

    vue-meta

---
+++ pages/event/crear.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Crear un evento</h1>
+  </div>
+</template>
+<script>
+export default{
+  head(){
+    return{
+      title: 'Crear Evento',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Crear un evento en tu vecindario'
+        }
+      ]
+    }
+  }
+}
+</script>
